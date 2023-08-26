8d4a759f9e49be8a43953eb5a517d72cffebb57f
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-08-26 06:37:29 +0200

    1.0 GuiButton doc

--- src/main/java/github/magyarzoli/app/swing/GuiButton.java
+++ src/main/java/github/magyarzoli/app/swing/GuiButton.java
@@ -3,10 +3,36 @@ package github.magyarzoli.app.swing;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * GuiButton class creates custom button components with specific properties, such as appearance, position, and
+ * behavior. By extending JButton and providing a custom constructor, you're able to create buttons with a consistent
+ * look and feel throughout your graphical user interface.
+ * @since       1.0
+ * @author      <a href=https://github.com/MagyarZoli>Magyar Zoltán</a>
+ */
 public class GuiButton
 extends JButton {
 
-    GuiButton(int fontSize){
+    /**
+     * {@code GuiButton} that extends the {@link javax.swing.JButton JButton} class. This class creates a custom button
+     * with specific properties and
+     * behaviors.
+     * <ul>
+     *     <li>The constructor definition for your {@code GuiButton} class. It takes an int parameter named
+     *     {@code fontSize}.</li>
+     *     <li>Sets the text of the button to "Start".</li>
+     *     <li>Sets the bounds (position and size) of the button. It will be positioned at {@code (x=390, y=110)} with
+     *     a width of <i>80</i> and a height of <i>40</i>.</li>
+     *     <li>Makes the button not focusable, preventing it from receiving focus when the user interacts with it.</li>
+     *     <li>Sets the background color of the button to the value defined in the {@code GuiColor} class under the
+     *     {@code BUTTON_BACKGROUND} constant.</li>
+     *     <li>Prevents the button from painting its border.</li>
+     *     <li>Sets the font of the button's text to "OCR A Extended", with a bold style and the specified
+     *     {@code fontSize}.</li>
+     * </ul>
+     * @param       fontSize specified font size.
+     */
+    GuiButton(int fontSize) {
         this.setText("Start");
         this.setBounds(390, 110, 80, 40);
         this.setFocusable(false);
--- src/main/java/github/magyarzoli/app/swing/GuiColor.java
+++ src/main/java/github/magyarzoli/app/swing/GuiColor.java
@@ -2,25 +2,30 @@ package github.magyarzoli.app.swing;
 
 import java.awt.Color;
 
+/**
+ * The class contains static fields that hold predefined color values.
+ * @since       1.0
+ * @author      <a href=https://github.com/MagyarZoli>Magyar Zoltán</a>
+ */
 public final class GuiColor {
 
     /**
-     * button color
+     * BUTTON_BACKGROUND with a {@link java.awt.Color Color} instance representing a shade of blue.
      */
     public final static Color BUTTON_BACKGROUND = new Color (0x5c5cd6);
 
     /**
-     * color of letters
+     * FONT_FOREGROUND with a {@link java.awt.Color Color} instance representing a light gray color.
      */
     public final static Color FONT_FOREGROUND = new Color (0xd9d9d9);
 
     /**
-     * color of panels
+     * FONT_BACKGROUND with a {@link java.awt.Color Color} instance representing a darker gray color.
      */
     public final static Color FONT_BACKGROUND = new Color (0x1e1e1e);
 
     /**
-     * background color
+     * COMPONENT_BACKGROUND with a {@link java.awt.Color Color} instance representing a very dark gray color.
      */
     public final static Color COMPONENT_BACKGROUND = new Color (0x121212);
 }
